b80f8acd73b3cc060485a290731d315d4abcee8e
Zacomo <giacomo.puligheddu@hotmail.it>
2019-11-26 11:16:05 +0100

    Introduzione di elementi per la recyclerview. Non ancora funzionante.

---
+++ app/src/main/java/com/zacomo/istentu/RecyclerViewAdapter.java
@@ -0,0 +1,45 @@
+package com.zacomo.istentu;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nomeTask;
+        LinearLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nomeTask = itemView.findViewById(R.id.nomeTask);
+            parentLayout = itemView.findViewById(R.id.parentLayout);
+
+
+
+        }
+    }
+}
