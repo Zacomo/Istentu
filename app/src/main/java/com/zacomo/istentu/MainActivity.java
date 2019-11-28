bca11f641a056cce1fc6be8e7642ee8f85989eff
Zacomo <giacomo.puligheddu@hotmail.it>
2019-11-28 12:14:48 +0100

    RecyclerView funzionante con stringhe di prova.

--- app/src/main/java/com/zacomo/istentu/MainActivity.java
+++ app/src/main/java/com/zacomo/istentu/MainActivity.java
@@ -1,14 +1,80 @@
 package com.zacomo.istentu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> mNomeTask = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "OnCreate: started");
+
+        sampleText();
+        initRecyclerView();
     }
+
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: initRecyclerView");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNomeTask);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private void sampleText(){
+        mNomeTask.add("Linea1");
+        mNomeTask.add("Linea2");
+        mNomeTask.add("Linea3");
+        mNomeTask.add("Linea4");
+        mNomeTask.add("Linea5");
+        mNomeTask.add("Linea6");
+        mNomeTask.add("Linea7");
+        mNomeTask.add("Linea8");
+        mNomeTask.add("Linea9");
+        mNomeTask.add("Linea10");
+        mNomeTask.add("Linea11");
+        mNomeTask.add("Linea12");
+        mNomeTask.add("Linea13");
+        mNomeTask.add("Linea14");
+        mNomeTask.add("Linea15");
+        mNomeTask.add("Linea16");
+        mNomeTask.add("Linea17");
+        mNomeTask.add("Linea18");
+        mNomeTask.add("Linea19");
+        mNomeTask.add("Linea20");
+        mNomeTask.add("Linea21");
+        mNomeTask.add("Linea22");
+        mNomeTask.add("Linea23");
+        mNomeTask.add("Linea24");
+        mNomeTask.add("Linea25");
+        mNomeTask.add("Linea26");
+        mNomeTask.add("Linea27");
+        mNomeTask.add("Linea28");
+        mNomeTask.add("Linea29");
+        mNomeTask.add("Linea30");
+        mNomeTask.add("Linea31");
+        mNomeTask.add("Linea32");
+        mNomeTask.add("Linea33");
+        mNomeTask.add("Linea34");
+        mNomeTask.add("Linea35");
+        mNomeTask.add("Linea36");
+    }
+
+
 }
--- app/src/main/java/com/zacomo/istentu/RecyclerViewAdapter.java
+++ app/src/main/java/com/zacomo/istentu/RecyclerViewAdapter.java
@@ -1,30 +1,58 @@
 package com.zacomo.istentu;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
     private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<String> mNomeTask = new ArrayList<String>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> nomeTask) {
+        this.mNomeTask = nomeTask;
+        this.mContext = context;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
 
+        holder.nomeTask.setText(mNomeTask.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + mNomeTask.get(position));
+
+                Toast.makeText(mContext, mNomeTask.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mNomeTask.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
