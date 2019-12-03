fa152cf2f233247800a4a1cd5130926ace1dd0a0
Zacomo <giacomo.puligheddu@hotmail.it>
2019-12-03 12:55:35 +0100

    Aggiunta la classe Task con nome, descrizione, priorità e data.

---
+++ app/src/main/java/com/zacomo/istentu/Task.java
@@ -0,0 +1,72 @@
+package com.zacomo.istentu;
+
+import java.util.Calendar;
+
+public class Task {
+
+    private String taskName;
+    private String taskDescription;
+    private int taskPriority;
+    private Calendar taskDue;
+
+    public Task(String taskName, String taskDescription, int taskPriority, Calendar taskDue){
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+
+        //La priorità sarà da 1 a 5
+        if (taskPriority > 0 && taskPriority < 6)
+            this.taskPriority = taskPriority;
+        else
+            this.taskPriority = 1;
+
+        //La data non può essere precedente ad oggi
+        if (taskDue.before(Calendar.getInstance().getTime()))
+            this.taskDue = Calendar.getInstance();
+        else
+            this.taskDue = taskDue;
+    }
+
+    public String getTaskName(){
+        return taskName;
+    }
+
+    public int getTaskPriority(){
+        return taskPriority;
+    }
+
+    public void setTaskName(String taskName){
+        this.taskName = taskName;
+    }
+
+    public boolean setTaskPriority(int taskPriority){
+
+        boolean done = false;
+
+        if (taskPriority > 0 && taskPriority < 6){
+            this.taskPriority = taskPriority;
+            done = true;
+        }
+        return done;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+
+    public Calendar getTaskDue() {
+        return taskDue;
+    }
+
+    public void setTaskDue(Calendar taskDue) {
+        if (taskDue.before(Calendar.getInstance().getTime()))
+            this.taskDue = Calendar.getInstance();
+        else
+            this.taskDue = taskDue;
+    }
+
+}
