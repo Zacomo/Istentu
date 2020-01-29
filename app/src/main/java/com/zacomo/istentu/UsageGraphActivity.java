6e1f341bf4ce6275db2794cd8d185e8bd7ab1f2c
Zacomo <giacomo.puligheddu@hotmail.it>
2020-01-29 16:19:59 +0100

    Introduzione grafico per stato task e spinner per visualizzare anche grafico priorità.

--- app/src/main/java/com/zacomo/istentu/UsageGraphActivity.java
+++ app/src/main/java/com/zacomo/istentu/UsageGraphActivity.java
@@ -3,7 +3,12 @@ package com.zacomo.istentu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.anychart.APIlib;
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
@@ -16,7 +21,7 @@ import java.util.Arrays;
 
 public class UsageGraphActivity extends AppCompatActivity {
 
-    AnyChartView anyChartView;
+    AnyChartView anyChartViewPriority, anyChartViewStatus;
     ArrayList<Task> mTasks;
 
     @Override
@@ -27,14 +32,40 @@ public class UsageGraphActivity extends AppCompatActivity {
         //forse devo passare context di main activity
         TaskFileHelper taskFileHelper = new TaskFileHelper(this);
         mTasks = taskFileHelper.readData("TaskList");
+        Spinner spinner = findViewById(R.id.spinnerChartType);
+        anyChartViewPriority = findViewById(R.id.anyChartViewPriority);
+        anyChartViewStatus = findViewById(R.id.anyChartViewStatus);
 
-        anyChartView = findViewById(R.id.anyChartView);
+        if (mTasks.size() > 0) {
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    switch (position){
+                        case 0:
+                            setUpPriorityPieChart();
+                            break;
+                        case 1:
+                            setUpTaskStatusPieChart();
+                            break;
+                        default:
+                            break;
+                    }
+                }
 
-        if (mTasks.size() > 0)
-            setUpPieChart();
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
     }
 
-    private void setUpPieChart(){
+    private void setUpPriorityPieChart(){
+        anyChartViewStatus.setVisibility(View.INVISIBLE);
+        anyChartViewPriority.setVisibility(View.VISIBLE);
+
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewPriority);
+
         Pie pie = AnyChart.pie();
         int[] numberTasksByPriority = new int[5];
         Arrays.fill(numberTasksByPriority, 0);
@@ -46,9 +77,66 @@ public class UsageGraphActivity extends AppCompatActivity {
             numberTasksByPriority[mTasks.get(i).getTaskPriority()-1]++;
 
         for (int i = 0; i < 5; i++)
-            dataEntries.add(new ValueDataEntry(i,numberTasksByPriority[i]));
+            dataEntries.add(new ValueDataEntry("P: "+ Integer.toString(i+1),numberTasksByPriority[i]));
 
+        pie.labels(true);
+        pie.title("Distribuzione dei task per priorità (P)");
+        pie.legend().title("Priorità");
+        pie.legend().align("top");
+        pie.legend().maxWidth("30%");
+        pie.legend().itemsLayout("verticalExpandable");
+        pie.legend().position("left");
+        pie.legend().paginator().orientation("bottom");
         pie.data(dataEntries);
-        anyChartView.setChart(pie);
+
+        anyChartViewPriority.setChart(pie);
+    }
+
+    private void setUpTaskStatusPieChart(){
+        anyChartViewPriority.setVisibility(View.INVISIBLE);
+        anyChartViewStatus.setVisibility(View.VISIBLE);
+        
+        APIlib.getInstance().setActiveAnyChartView(anyChartViewStatus);
+
+        Pie pie = AnyChart.pie();
+        int[] numberTasksByStatus = new int[3];
+        Arrays.fill(numberTasksByStatus, 0);
+
+        ArrayList<DataEntry> dataEntries = new ArrayList<>();
+
+        //con questo for conto quanti task di una determinata priorità ci sono
+        for (int i = 0; i < mTasks.size(); i++){
+            switch (mTasks.get(i).getTaskStatus()){
+                case 0: //task in attesa
+                    numberTasksByStatus[0]++;
+                    break;
+                case 1: //task in corso
+                    numberTasksByStatus[1]++;
+                    break;
+                case 2: //task completato
+                    numberTasksByStatus[2]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        dataEntries.add(new ValueDataEntry("Status: in attesa",numberTasksByStatus[0]));
+        dataEntries.add(new ValueDataEntry("Status: in corso",numberTasksByStatus[1]));
+        dataEntries.add(new ValueDataEntry("Status: completato",numberTasksByStatus[2]));
+
+        pie.labels(true);
+        pie.title("Distribuzione dei task per stato");
+        pie.legend().title("Stato");
+        pie.legend().align("top");
+        pie.legend().maxWidth("40%");
+        pie.legend().itemsLayout("verticalExpandable");
+        pie.legend().position("left");
+        pie.legend().paginator().orientation("bottom");
+        pie.data(dataEntries);
+
+        anyChartViewStatus.setChart(pie);
+
+        Toast.makeText(this, "Helo", Toast.LENGTH_SHORT).show();
     }
 }
